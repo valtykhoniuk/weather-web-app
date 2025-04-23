62b549df9de5e2465ad597ebd1db20382670d051
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-04-23 23:10:06 +0200

    base html/css added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,70 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      city: "",
+    };
+  },
+};
+</script>
 
-<template></template>
+<template>
+  <div class="wrapper">
+    <h1>Weather app</h1>
+    <p>Get known weather in {{ city }}</p>
+    <input
+      type="text"
+      v-on:input="this.city = $event.target.value"
+      placeholder="Write the name"
+    />
+    <button>Go!</button>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  padding: 20px;
+  background-color: whitesmoke;
+  text-align: center;
+}
+
+.wrapper h1 {
+  margin-top: 50px;
+}
+
+.wrapper p {
+  margin-top: 20px;
+}
+
+.wrapper input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #ff9a3b;
+  color: #ff9a3b;
+  padding: 5px 8px;
+  outline: none;
+}
+
+.wrapper input:focus {
+  border-bottom: 2px solid #e77104;
+}
+
+.wrapper button {
+  background: #ff9a3b;
+  color: whitesmoke;
+  border-radius: 10px;
+  padding: 10px;
+  border: none;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+
+.wrapper button:hover {
+  transform: scale(1.1);
+}
+</style>
