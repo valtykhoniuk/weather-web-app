11b467527bd957ad542e69f8bb85754130ac4a71
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-04-24 00:26:15 +0200

    fixes for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "/weather-web-app/" : "/",
+  base: "/weather-web-app/",
 });
